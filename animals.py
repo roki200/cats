02a576bca512ded30fd54147cf2cf14407f5c96e
Roki_Crazy <89066095+roki200@users.noreply.github.com>
2022-01-01 17:53:04 +0700

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,15 @@
+import requests
+import random
+from config import cap_mas, dog_mas
+
+class Animals():
+	def give_me_a_cat():
+		caption = cap_mas[int(random.uniform(0, len(cap_mas)))]
+		json = requests.get("https://api.thecatapi.com/v1/images/search").json()
+		cat = json[0]['url']
+		return [cat, caption]
+	def give_me_a_dog():
+		caption = dog_mas[int(random.uniform(0, len(dog_mas)))]
+		json = requests.get("https://dog.ceo/api/breeds/image/random").json()
+		dog = json['message']
+		return [dog, caption]
